36afc4c3b20bd00c7bd6b57045cbfa2f6e7e8e74
FARAJI <farajinahid2@gmail.com>
2026-06-09 17:00:54 +0600

    learn the usages of strip(), title() & split() method

--- python/hello.py
+++ python/hello.py
@@ -2,6 +2,7 @@
 
 """
 This is a comment!
+strip() is a method that removes any leading and trailing whitespace from the input string. This ensures that if the user accidentally adds extra spaces before or after their name, it won't affect the greeting.
 """
-name = input("what's your name? ")
-print('hello,',name)
+first, last = input("what's your name? ").strip().title().split(" ")
+print(f"hello, {first}!")
